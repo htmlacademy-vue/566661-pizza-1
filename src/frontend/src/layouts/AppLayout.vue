ec3f5f5cd5ef113cb8d6a5123531227e4f8dbc79
Anton Kondratev <kondratev@elma-bpm.com>
2022-07-19 07:03:55 +0400

    Создана страница статическая

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppLayoutHeader />
+    <IndexHome />
+  </div>
+</template>
+
+<script>
+import AppLayoutHeader from "@/layouts/AppLayoutHeader";
+import IndexHome from "@/views/Index";
+export default {
+  name: "AppLayout",
+  components: {
+    AppLayoutHeader,
+    IndexHome,
+  },
+};
+</script>
+
+<style scoped></style>
